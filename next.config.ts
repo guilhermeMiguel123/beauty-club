a154443d88c803838d483d837a69c0c0264f1b7b
Guilherme Miguel <guilherme12miguel123@gmail.com>
2026-08-26 01:11:44 -0300

    fix: limpando arquivos residuais e ajustando config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Ignora erros chatos de lint e TypeScript apenas na hora do build da Vercel
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-};
+} as any;
 
 export default nextConfig;
